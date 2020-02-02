00b7665aaa91b15700d499fd8b0ef610c60148a9
Valmir Franca <vfranca3@gmail.com>
2020-02-01 23:03:27 -0300

    Implementada funcao que recupera puts do ativo

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,5 +1,13 @@
 from options import __version__
+from options import options
+from unittest import mock
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+@mock.patch("options.options.mql5")
+def test_pega_todas_as_puts_de_um_ativo(mql5):
+    mql5.iClose.return_value = 0.19
+    assert options.puts("bova11", 96, 99) == ["bovao96", "bovao97", "bovao98"]
