bdf50d20ef00aa0c445c20f6afd91370b9ac60de
Valmir Franca <vfranca3@gmail.com>
2020-02-02 22:40:07 -0300

    Implementado comando option series para exibir todas as series de opcoes

--- mtoptions/cli.py
+++ mtoptions/cli.py
@@ -2,6 +2,12 @@ import click
 from mtoptions import options
 
 
+@click.group()
+def option():
+    """Console de scripts para MTOptions."""
+    pass
+
+
 @click.command()
 @click.argument("symbol")
 @click.argument("start", type=int)
@@ -13,3 +19,17 @@ def puts(symbol, start, limit):
         price = options.price(symbol)
         click.echo("%s %.2f" % (symbol, price))
     return 0
+
+
+@click.command()
+@click.argument("type")
+def series(type):
+    """Exibe as séries das opções call e put."""
+    all_series = options.get_series(type)
+    for month in all_series:
+        click.echo("%s %s" % (all_series[month], month))
+    return 0
+
+
+option.add_command(puts)
+option.add_command(series)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 from mtoptions import cli
 from click.testing import CliRunner
 from unittest import mock, skip
+from .fixtures import series
 
 
 runner = CliRunner()
@@ -12,4 +13,13 @@ def test_exibe_as_puts_do_ativo(mql5):
     mql5.iClose.return_value = 0.19
     res = runner.invoke(cli.puts, ["bova11", 96, 99])
     assert res.output == "bovao96\nbovao98\n"
+    assert res.exit_code == 0
+
+
+def test_exibe_todas_as_series_do_tipo_de_opcao():
+    res = runner.invoke(cli.option, ["series", "put"])
+    expec = ""
+    for month in series["put"]:
+        expec += "%s %s\n" % (series["put"][month], month)
+    assert res.output == expec
     assert res.exit_code == 0
